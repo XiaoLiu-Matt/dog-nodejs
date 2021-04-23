0dd0984d84c3f00a7b776868e0aa8343a44ccbb3
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-04-23 19:06:54 -0400

    more allows

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(session({
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin, Accept');
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.header('Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Credentials", "true");
