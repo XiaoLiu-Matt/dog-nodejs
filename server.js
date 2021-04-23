2d595d9b3b8ea6c360cf3474e5c2051d09772b08
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-04-23 14:34:32 -0400

    connect to cloud database

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-require('./database')()
-
+// require('./database')()
+// const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+var MongoClient = require('mongodb').MongoClient;
+
+var username = 'YOUR_USERNAME';
+var password = 'YOUR_PASSWORD';
+var hosts = 'iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889';
+var database = 'DOG';
+var options = '?replicaSet=e32099f5c50d45138556f82ad258847e';
+var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+MongoClient.connect(connectionString, function(err, db) {
+    if (db) {
+        db.close();
+    }
+    if (err) {
+        console.log('Error: ', err);
+    } else {
+        console.log('Connected!');
+        process.exit();
+    }
+});
+
+// mongoose.connect('mongodb://YOUR_USERNAME:YOUR_PASSWORD@iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889/DOG?replicaSet=e32099f5c50d45138556f82ad258847e')
 const session = require('express-session')
 app.use(session({
     secret: 'keyboard cat',
@@ -13,7 +37,7 @@ app.use(session({
     saveUninitialized: true,
     // cookie: { secure: true }
 }))
-
+// mongodb://YOUR_USERNAME:YOUR_PASSWORD@iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889/YOUR_DATABASE_NAME?replicaSet=e32099f5c50d45138556f82ad258847e
 // Configures CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -26,11 +50,10 @@ app.use(function (req, res, next) {
 });
 require('./controllers/user-controller')(app)
 require('./controllers/comments-controller')(app)
-// app.get('/test', async (req, res) => {
-//     try {
-//         res.send('hello world');
-//     } catch (err) {
-//         console.log(err)
-//     }
-// })
-app.listen(4001)
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+});
+// app.listen(4001)
