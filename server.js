1b4d993ef94dacc1978be40e708d7f31cfa3a3c9
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-04-23 21:15:07 -0400

    clean codes

--- server.js
+++ server.js
@@ -7,29 +7,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-// var MongoClient = require('mongodb').MongoClient;
-//
-// var username = 'YOUR_USERNAME';
-// var password = 'YOUR_PASSWORD';
-// var hosts = 'iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889';
-// var database = 'DOG';
-// var options = '?replicaSet=e32099f5c50d45138556f82ad258847e';
-// var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
-//
-// MongoClient.connect(connectionString, function(err, db) {
-//     if (db) {
-//         db.close();
-//     }
-//     if (err) {
-//         console.log('Error: ', err);
-//     } else {
-//         console.log('Connected!');
-//         process.exit();
-//     }
-// });
-
-// mongoose.connect('mongodb://YOUR_USERNAME:YOUR_PASSWORD@iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889/DOG?replicaSet=e32099f5c50d45138556f82ad258847e')
 const session = require('express-session')
 app.use(session({
     secret: 'keyboard cat',
@@ -37,7 +14,7 @@ app.use(session({
     saveUninitialized: true,
     // cookie: { secure: true }
 }))
-// mongodb://YOUR_USERNAME:YOUR_PASSWORD@iad2-c7-0.mongo.objectrocket.com:53889,iad2-c7-2.mongo.objectrocket.com:53889,iad2-c7-1.mongo.objectrocket.com:53889/YOUR_DATABASE_NAME?replicaSet=e32099f5c50d45138556f82ad258847e
+
 // Configures CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -56,4 +33,3 @@ const port = process.env.PORT || 4001;
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}/`);
 });
-// app.listen(4001)
