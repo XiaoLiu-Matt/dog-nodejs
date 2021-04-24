8a7c41970dae3f24b5d85a5d132e85cb6fd14338
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-04-23 23:03:16 -0400

    original

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(session({
 }))
 
 // Configures CORS
-var cors = require('cors');
-app.use(cors());
+// var cors = require('cors');
+// app.use(cors());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     // res.header('Access-Control-Allow-Origin', 'https://dog-react-client.herokuapp.com');
